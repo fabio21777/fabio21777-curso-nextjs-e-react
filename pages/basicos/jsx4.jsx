6dcd8baad7cb91318ee13546d24400cbb97d345b
fabio21777 <fabiosouza21777@gmail.com>
2021-08-17 16:35:02 -0300

    desmostrando integração entre jsx e js

---
+++ pages/basicos/jsx4.jsx
@@ -0,0 +1,9 @@
+export default function JSX4() {
+    const subTitulo = "Estou no JS"
+    return(
+        <div>
+            <h1>Integração entre js e jsx</h1>
+            <h2>{subTitulo}</h2>
+        </div>
+    )
+}
