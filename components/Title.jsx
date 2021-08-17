fb32407bd3abbd84cc7178d30295c85bcc1f4688
fabio21777 <fabiosouza21777@gmail.com>
2021-08-17 17:24:52 -0300

    utilizando components

---
+++ components/Title.jsx
@@ -0,0 +1,8 @@
+export default function Title() {
+    return (
+        <div>
+            <h1>Titulo</h1>
+            <h2>subTitulo</h2>
+        </div>
+    )
+}
---
+++ pages/basicos/usingTitle.jsx
@@ -0,0 +1,10 @@
+import Title from "../../components/title"
+export default function usingTitle() {
+    return (
+        <div>
+            <Title/>
+            <Title/>
+            <Title/>
+        </div>
+    )
+}
