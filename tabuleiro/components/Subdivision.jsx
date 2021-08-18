d8ecce81d89cf1ca7f66aec0e733ff52fa28079f
fabio21777 <fabiosouza21777@gmail.com>
2021-08-18 17:31:30 -0300

    adicionando componente subdivisão

---
+++ tabuleiro/components/Subdivision.jsx
@@ -0,0 +1,7 @@
+import styleSubDivision from '../styles/subDivision.module.css'
+export default function subDivision(props) {
+    return(
+        <div className = {styleSubDivision.subDivision}>
+        </div>
+    )
+}
---
+++ tabuleiro/pages/home.jsx
@@ -0,0 +1,10 @@
+import styles from "../styles/home.module.css"
+import SubDivision from "../components/Subdivision"
+export default function home() { 
+    return(
+        <div className = {styles.container}>
+            <SubDivision/>git reset 
+        </div>
+    )
+
+}
